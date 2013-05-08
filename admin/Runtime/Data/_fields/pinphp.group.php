dce93df88fe347fda6d7dbfa6151411fdc2a962f
yutao <tao6yu@163.com>
2013-05-08 19:25:50 +0800

    first blood

--- admin/Runtime/Data/_fields/pinphp.group.php
+++
@@ -1,2 +0,0 @@
-<?php
-return array ( 0 => 'id', 1 => 'name', 2 => 'title', 3 => 'create_time', 4 => 'update_time', 5 => 'status', 6 => 'sort', '_autoinc' => true, '_pk' => 'id', ); ?>
--- config.inc.php
+++ config.inc.php
@@ -1,15 +1,14 @@
 <?php
 return array (
-  // 'DB_HOST' => '127.0.0.1',
   'DB_HOST' => 'localhost',	
-  'DB_NAME' => 'pinphp',
+  'DB_NAME' => 'cnupin',
   'DB_USER' => 'root',
-  // 'DB_PWD' => 'root',
   'DB_PWD' => 'upin@admin',
   'DB_PORT' => '3306',
-  'DB_PREFIX' => 'pp_',
+  'DB_PREFIX' => 'up_',
   'DEFAULT_THEME' => 'default',
   'TMPL_ACTION_SUCCESS' => 'public:success',
   'TMPL_ACTION_ERROR' => 'public:error',
 ); 
 ?>
+
